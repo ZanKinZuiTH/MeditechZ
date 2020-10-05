bf30518c9b255b8d65802a5c7458c71fcd764b5e
gun191 <freeman263@gmail.com>
2020-10-05 14:28:36 +0700

    แก้เรื่อง select lab

--- MediTechData/MediTech.Model/Lab/ResultComponentModel.cs
+++ MediTechData/MediTech.Model/Lab/ResultComponentModel.cs
@@ -132,7 +132,7 @@ namespace MediTech.Model
                     }
                     else
                     {
-                        IsAbnormal = "";
+                        IsAbnormal = null;
                     }
                 }
             }
