132675dc66e9f0203708a27e94d3cbfed4a64efe
decemyg <gracenatthakan@gmail.com>
2023-11-09 12:38:13 +0700

    เพิ่มใบรับรองแพทย์ให้ mobile

--- MediTech/MediTech/Helpers/ConstantData.cs
+++ MediTech/MediTech/Helpers/ConstantData.cs
@@ -25,6 +25,7 @@ namespace MediTech.Helpers
             ListPatientReports.Add(new ReportsModel { Name = "ใบรับรองแพทย์ 2 ส่วน (ภาษาอังกฤษ)", NamespaceName = "MediTech.Reports.Operating.Patient.MedicalCertificateEng2Parts" });
             ListPatientReports.Add(new ReportsModel { Name = "ใบรับรองรังสีแพทย์", NamespaceName = "MediTech.Reports.Operating.Patient.RadilogyCertificate" });
             ListPatientReports.Add(new ReportsModel { Name = "ใบรับรองแพทย์ที่อับอากาศ", NamespaceName = "MediTech.Reports.Operating.Patient.ConfinedSpaceCertificate1" });
+            ListPatientReports.Add(new ReportsModel { Name = "ใบรับรองแพทย์ที่อับอากาศ (Mobile)", NamespaceName = "MediTech.Reports.Operating.Patient.ConfinedSpaceCertificate1" });
             ListPatientReports.Add(new ReportsModel { Name = "ใบรับรองแพทย์ทำงานบนที่สูง", NamespaceName = "MediTech.Reports.Operating.Patient.WorkingHeightCertificate1" });
             ListPatientReports.Add(new ReportsModel { Name = "ใบรับรองแพทย์ทำงานบนที่สูง (ภาษาอังกฤษ)", NamespaceName = "MediTech.Reports.Operating.Patient.WorkingHeightCertificateEng1" });
             ListPatientReports.Add(new ReportsModel { Name = "ใบรับรองแพทย์ Work Permit", NamespaceName = "MediTech.Reports.Operating.Patient.WorkPermitCertificate" });
